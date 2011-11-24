e79c13e903c3cf532ea13ffe3af6aab4279773ec
Michael Heap <m@michaelaheap.com>
2011-11-24 12:20:02 +0000

    Initial Commit

---
+++ Message.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Message
+ * 
+ * @package Message
+ * @author Michael Heap
+ */
+class Message {
+	
+	protected static $_messages = array();
+
+	/**
+	 * Message::_init()
+	 * 
+	 */
+	public static function _init()
+	{
+		$current = Session::get('_messages');
+        
+		if ($current)
+		{
+			self::$_messages = unserialize($current);
+		}
+		else
+		{
+			self::reset();
+		}
+	}
+
+	/**
+	 * Message::notice()
+	 * 
+	 * @param string $m
+	 */
+	public static function notice($m)
+	{
+		self::add('notice', $m);
+	}
+
+	/**
+	 * Message::error()
+	 * 
+	 * @param string $m
+	 */
+	public static function error($m)
+	{
+		self::add('error', $m);
+	}
+
+	/**
+	 * Message::info()
+	 * 
+	 * @param string $m
+	 */
+	public static function info($m)
+	{
+		self::add('info', $m);
+	}
+
+	/**
+	 * Message::add()
+	 * 
+	 * @param string $type (notice, error, info)
+	 * @param string $message
+	 */
+	protected static function add($type, $message)
+	{
+		self::$_messages->{$type}[] = $message;
+		self::write();
+	}
+
+	/**
+	 * Message::get()
+	 * 
+	 */
+	public static function get()
+	{
+		$messages = self::$_messages;
+		foreach ($messages as $k => $v)
+		{
+			if ( ! count($v))
+			{
+				unset($messages->{$k});
+			}
+		}
+
+		self::reset();
+		self::write();
+
+		return count((array) $messages) == 0 ? null : $msgs;
+	}
+
+	/**
+	 * Message::reset()
+	 * 
+	 */
+	public static function reset()
+	{
+		self::$_messages = (object) array(
+			'notice' => array(),
+			'error' => array(),
+			'info' => array()
+		);
+	}
+
+	/**
+	 * Message::write()
+	 * 
+	 */
+	public static function write()
+	{
+		Session::set('_messages', serialize(self::$_messages));
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Fuel-Message
+============
+
+A flashdata wrapper for FuelPHP.
+
+Usage:
+
+Message::Notice("This is a notice");
+Message::Get(); => 
+
+stdClass {
+	"notice" -> array("This is a notice");
+}
